17503706edf38b6c5de803ec78ea6624bcd61e53
Justin <justinyeh13@gmail.com>
2025-03-23 13:24:29 -0400

    Added Outlets and Modified Application with Labels and Buttons

--- A2_iOS_Justin_101400171/ViewController.swift
+++ A2_iOS_Justin_101400171/ViewController.swift
@@ -6,6 +6,13 @@ import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productDescription: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var productProvider: UILabel!
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
