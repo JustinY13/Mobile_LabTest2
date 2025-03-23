3a48f954d14fcbd73109fd8a9e5ebd38a0f1cbca
Justin <justinyeh13@gmail.com>
2025-03-23 14:17:16 -0400

    Created functions to show and fetch products

--- A2_iOS_Justin_101400171/ViewController.swift
+++ A2_iOS_Justin_101400171/ViewController.swift
@@ -13,11 +13,51 @@ class ViewController: UIViewController {
     @IBOutlet weak var previousButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
+    var products: [Product] = []
+    var mainIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getProducts()
+        showProduct()
     }
-
-
+    
+    func getProducts() {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let getRequest: NSFetchRequest<Product> = Product.fetchRequest()
+        
+        do {
+            products = try context.fetch(getRequest)
+        } catch {
+            print("Fetching products failed: \(error)")
+        }
+    }
+    
+    func showProduct() {
+        guard products.count > 0 else { return }
+        let product = products[mainIndex]
+        productName.text = product.productName
+        productDescription.text = product.productDescription
+        productPrice.text = String(product.productPrice)
+        productProvider.text = product.productProvider
+        
+        previousButton.isEnabled = mainIndex > 0
+        nextButton.isEnabled = mainIndex < products.count - 1
+    }
+    
+    @IBAction func nextProduct(_ sender: UIButton) {
+        if mainIndex < products.count - 1 {
+            mainIndex = mainIndex + 1
+            showProduct()
+        }
+    }
+    
+    @IBAction func previousProduct(_ sender: UIButton) {
+        if mainIndex > 0 {
+            mainIndex = mainIndex - 1
+            showProduct()
+        }
+    }
+    
 }
 
